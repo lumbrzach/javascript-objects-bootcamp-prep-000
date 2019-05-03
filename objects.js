f108678e7dc85a3284dbf586451828ca9c6674f1
Zach Parker <parkerz410@gmail.com>
2019-05-03 15:01:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,12 +1,11 @@
 var playlist = {artist: "song"};
 
-function updatePlaylist (playlist, artist, song) {
-  playlist[artist]=song;
+function updatePlaylist (playlist, artistName, song) {
+  playlist[artistName]=song;
   return playlist;
 };
 
-function removeFromPlaylist (playlist, artist) {
-  var artist = 'Slowdive';
-  delete playlist.artist;
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
   return playlist;
 };
